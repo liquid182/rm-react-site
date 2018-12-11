c6c742bccb81c9530cd587c7e93ad268a2dc8fe3
liquid182 <mccullough.ryan@gmail.com>
2018-12-11 13:50:25 -0800

    RM: Adding exact key to home

--- src/components/navigation/NavItem.tsx
+++ src/components/navigation/NavItem.tsx
@@ -22,13 +22,17 @@ export class NavItem extends React.Component<INavItem, {}> {
     }
 
     public render(){
-        return (
-            <li key={this.navItem.key} className={this.navItemClass}>
-                <NavLink
-                    className="nav-link" to={this.navItem.url}>{this.navItem.label}
-                </NavLink>
-            </li>
-        );
+      let exact:boolean = false;
+      if( this.navItem.url === "/" ){
+        exact=true;
+      }
+      return (
+        <li key={this.navItem.key} className={this.navItemClass}>
+            <NavLink
+                className="nav-link" to={this.navItem.url} exact={exact}>{this.navItem.label}
+            </NavLink>
+        </li>
+      );
     }
 
 }
